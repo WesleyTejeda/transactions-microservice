9b6b6fae4daaddc3067e7778add30d17eb2987c7
Wesley Tejeda <wtejeda@talentpath.com>
2022-01-11 17:55:25 -0500

    Updated routes with new controller functions

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,57 @@ import express from "express";
 const router = express.Router();
 
 router.get("/customers", (req, res)=> {
-    let customers = customerController.getCustomers(req, res);
+    customerController.getCustomers(req, res);
 })
 
-router.get("/wallets", (req, res)=> {
-    let wallets = customerController.getWallets(req, res);
+router.get("/customers/:id", (req, res) => {
+    customerController.getOneCustomer(req, res);
 })
 
-router.get("/transactions", (req, res)=> {
-    let transactions = customerController.getTransactions(req, res);
-})
-
-router.post("/create/customer", (req, res) => {
+router.post("/customers/create", (req, res) => {
     customerController.createCustomer(req, res);
 })
 
-router.post("/create/transaction", (req, res) => {
-    transactionController.createTransaction(req, res);
+router.patch("/customers/patch", (req, res) => {
+    customerController.updateCustomer(req, res);
 })
 
-router.post("/create/wallet", (req, res) => {
+router.get("/wallets", (req, res)=> {
+    walletController.getWallets(req, res);
+})
+
+router.get("/wallets/:id", (req, res) => {
+    walletController.getUserWallet(req, res);
+})
+
+
+router.post("/wallets/create", (req, res) => {
     walletController.createWallet(req, res);
 })
+
+router.patch("/wallets/deposit", (req, res) => {
+    walletController.depositWallet(req, res);
+})
+
+router.get("/transactions", (req, res)=> {
+    transactionController.getTransactions(req, res);
+})
+
+router.get("/transactions/:id", (req, res) => {
+    transactionController.getUserTransaction(req, res);
+})
+
+router.post("/transactions/create", (req, res) => {
+    transactionController.createTransaction(req, res).then(transaction => {
+        if(transaction.dataValues.type === "deposit") {
+            let wallet = walletController.depositWallet(req, res, transaction);
+            res.json({transaction: transaction, wallet: wallet});
+        } else if(transaction.dataValues.type === "purchase") {
+            let wallet = walletController.chargeWallet(req, res, transaction);
+            res.json({transaction: transaction, wallet: wallet});
+        }
+    })
+})
+
 
 export default router
