ee5027def6376c918d3a5efbb676ff12c7cd29b4
Wesley Tejeda <wtejeda@talentpath.com>
2022-01-13 09:34:53 -0500

    Implemented new routes

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get("/customers", (req, res)=> {
     customerController.getCustomers(req, res);
 })
 
-router.get("/customers/:id", (req, res) => {
+router.get("/customers/:username", (req, res) => {
     customerController.getOneCustomer(req, res);
 })
 
@@ -19,21 +19,8 @@ router.patch("/customers/patch", (req, res) => {
     customerController.updateCustomer(req, res);
 })
 
-router.get("/wallets", (req, res)=> {
-    walletController.getWallets(req, res);
-})
-
-router.get("/wallets/:id", (req, res) => {
-    walletController.getUserWallet(req, res);
-})
-
-
 router.post("/wallets/create", (req, res) => {
     walletController.createWallet(req, res);
-})
-
-router.patch("/wallets/deposit", (req, res) => {
-    walletController.depositWallet(req, res);
 })
 
 router.get("/transactions", (req, res)=> {
@@ -46,12 +33,24 @@ router.get("/transactions/:id", (req, res) => {
 
 router.post("/transactions/create", (req, res) => {
     transactionController.createTransaction(req, res).then(transaction => {
-        if(transaction.dataValues.type === "deposit") {
+        if(transaction){
+            if(transaction.dataValues.type === "deposit") {
+                let wallet = walletController.depositWallet(req, res, transaction);
+                res.json({transaction: transaction, wallet: wallet});
+            } else if(transaction.dataValues.type === "purchase") {
+                let wallet = walletController.chargeWallet(req, res, transaction);
+                res.json({transaction: transaction, wallet: wallet});
+            }
+        }
+    })
+})
+
+router.post("/transactions/sell", (req, res) => {
+    transactionController.sellFund(req, res).then(transaction => {
+        if(transaction){
+            console.log(transaction, "sell -----")
             let wallet = walletController.depositWallet(req, res, transaction);
-            res.json({transaction: transaction, wallet: wallet});
-        } else if(transaction.dataValues.type === "purchase") {
-            let wallet = walletController.chargeWallet(req, res, transaction);
-            res.json({transaction: transaction, wallet: wallet});
+            res.json({transaction: transaction, wallet: wallet})
         }
     })
 })
