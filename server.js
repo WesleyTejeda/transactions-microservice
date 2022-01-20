5565a7f566f7216ad80c9c9ebb59c1198502b032
Wesley Tejeda <wtejeda@talentpath.com>
2022-01-20 16:28:49 -0500

    Added cors to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import session from "express-session"
 import dotenv from "dotenv";
 import router from "./routes/index.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 4040;
@@ -19,6 +20,19 @@ app.use(session({
     saveUninitialized: true,
     cookie: { secure: false }
 }))
+
+const whitelist = ["http://localhost:4200"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 
 app.listen(port, () =>{
     console.log(`Server live and listening on port ${port}`);
